f47e7af80fb5360bbb7130f5753407143cf59a8e
Samyak Khatua <samyakkhatua4@gmail.com>
2022-08-08 00:56:28 +0530

    added styling to preview

--- index.js
+++ index.js
@@ -46,11 +46,69 @@ app.post('/link-preview', (req, res) => {
             
             console.log(data)
             // res.json(data)
-            res.send(`<a href="`+URLL+`"><span class="link"></span></a>
+            res.send(`
+            <div class="previewCard">
+            <a href="`+URLL+`"><span class="link"></span></a>
             <img class="urlImg" src="`+image+`" alt=""></img>
             <p class="urlTitle">`+title+`</p>
             <p class="urlDesc">`+description+`</p>
-            <p class="urlDomain">www.`+domain+`</p>`)
+            <p class="urlDomain">www.`+domain+`</p>
+            </div>
+            <style>
+            .previewCard{
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                max-width: 300px;
+                margin: auto;
+                font-family: arial;
+            
+                border: 5px solid #D9FDD3;
+                border-radius: 19px;
+            
+                background-color: #D1F4CC;
+                color: black;
+            }
+            
+            .link {
+                position: absolute;
+                width: 100px;
+                height: 100px;
+                top: 0;
+                left: 0;
+                z-index: 1;
+              }
+            
+            .urlImg{
+                width: 300px;
+                border-radius: 15px 15px 0px 0px;
+                padding: 0px;
+            }
+            
+            .urlTitle{
+                font-size: 18px;
+                font-weight: 550;
+                font-family:Arial, Helvetica, sans-serif;
+                line-height: 18px;
+            
+                padding: 0px 10px 0px 10px;
+            
+            }
+            
+            .urlDesc{
+                font-size: 14px;
+                font-family: Arial, Helvetica, sans-serif;
+                padding: 0px 10px 0px 10px;
+                line-height: 18px;
+                color: #393d3f;
+            }
+            
+            .urlDomain{
+                font-size: 12px;
+                line-height: 1px;
+                font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                padding: 0px 10px 5px 10px;
+                color: #667781;
+            }
+            </style>`)
 
         }).catch((err) => {
             return res.json({
